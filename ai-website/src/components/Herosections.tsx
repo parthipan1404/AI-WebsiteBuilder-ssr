d5f73f9cd7a9bebef73979c3cd13a7b70947daf8
parthipan1404 <parthipan.srinivasan@777hosting.net>
2025-01-08 10:46:01 +0530

    newone

--- ai-website/src/components/Herosections.tsx
+++ ai-website/src/components/Herosections.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
 
 const HeroPage = () => {
   return (
-    <div>
-    <div className=''>
-        <div className="row">
-            <div className="col-12 m-5">
-            <h4 className="m-2">777Builder AI</h4>
-            <h1 className="m-4">Build your free website, and we'll host it for you!</h1>
-            <p className="m-2">Start from scratch or bring your ideas to life—our AI tools make it easy.</p>
+    <div className='container'>
+    <div className="row ">
+            <h4 className="col-12 text-center py-2 mt-5">777Builder AI</h4>
+            <h1 className="col-12 text-center py-2">Build your free website, and we'll host it for you!</h1>
+            <p className="col-12 text-center">Start from scratch or bring your ideas to life—our AI tools make it easy.</p>
+            
+            
+            <div className="col-4 text-center">Free Hosting</div>
+            <div className="col-4 text-center">No credit card needed
             </div>
-            <div className='d-flex justify-content-center'>
-            <div className="col-3 ">Free Hosting</div>
-            <div className="col-3 ">No credit card needed
-            </div>
-            <div className="col-3">No coding required</div>
+            <div className="col-4 text-center">No coding required</div>
+            <div className="col-12 text-center">
+            <button className="btn btn-primary mt-3" style={{background: '#4CAF50',borderRadius:'60px',border:'none',boxShadow:'-4px -4px 5px 0px rgba(0, 0, 0, 0.25098039215686274) inset',padding:'10px 40px'}}>Get StartNow</button>
             </div>
         </div>
-    </div>
     </div>
   );
 };
