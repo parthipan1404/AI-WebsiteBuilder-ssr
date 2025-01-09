e3aae3cdb2f1bb320f84f857b591f15da16152f5
Suryaprasath Mani <msurya7305@gmail.com>
2025-01-09 15:24:24 +0530

    Host on us

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,8 +1,20 @@
-
+import "../index.css";
+import { Container, Row, Col } from "react-bootstrap";
 const Demo = () => {
- return( <div>
-  <h1>Hello World!</h1>
-  <h2>Welcome to my project!</h2>
+ return( 
+ <div className="">
+  <Container>
+    <Row>
+      <Col md={8} >
+      <h1>Host on us or on WordPress or Export to Code!</h1>
+      <p>You can publish the project to your own domain, or publish in WordPress. We help you grow your business and remove all hassles involved with hosting a website.</p>
+      </Col>
+      <Col md={4} className="mt-3">
+       
+      </Col>
+      
+    </Row>
+  </Container>
 </div>);
 };
 
