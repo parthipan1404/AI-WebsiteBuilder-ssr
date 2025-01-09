978a4e3a860a4b58d67c0fbcd6135ffa8a67ad99
parthipan1404 <parthipan.srinivasan@777hosting.net>
2025-01-09 14:11:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/https://github.com/parthipan1404/AI-WebsiteBuilder-ssr/",
   plugins: [react()],
 })
