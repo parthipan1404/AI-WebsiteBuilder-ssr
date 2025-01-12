515e4c5451d26f111b9770e586acb23052d64a0c
parthipan1404 <parthipan.srinivasan@777hosting.net>
2025-01-12 09:51:43 +0530

    lazy load added

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import HeroPage from "./components/Herosections";
-import Navbar from "./components/NavBar";
-import Footer from "./components/footer";
-import Layout from "./components/Layout";
-import Demo from "./components/Demo";
-import Faq from "./components/Faq";
-import AIWebsitePromo from "./components/AIWebsitePromo";
+import React,{lazy} from 'react';
+const MyComponent = lazy(()=>import('./pages/Index'))
 
 function App() {
+
   return (
     <>
-      <Navbar />
-      <HeroPage />
-      <Layout />
-      <Demo />
-      <AIWebsitePromo />
-      <Faq />
-      <Footer />
+      <MyComponent/>
     </>
   );
 }
