2713e9444fc09f33af61a30d02455666292d2dbe
parthipan1404 <parthipan.srinivasan@777hosting.net>
2025-01-22 17:41:14 +0530

    error sloved

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/parthipan1404/AI-WebsiteBuilder-ssr">
+    <BrowserRouter basename="/AI-WebsiteBuilder-ssr">
       <App />
     </BrowserRouter>
   </StrictMode>
